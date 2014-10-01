b3b533aae70d6d1de2f5143907d0cc942f7be6ae
Andriy Moroz <andriymoroz@gmail.com>
2014-10-01 22:17:52 +0300

    Fixed nrf_base. Can receive dht messages

--- nrf_base/nrf_base.cpp
+++ nrf_base/nrf_base.cpp
@@ -10,7 +10,7 @@
 
 
 // Setup for GPIO 25 CE and CE1 CSN with SPI Speed @ 8Mhz
-RF24 radio(RPI_V2_GPIO_P1_22, BCM2835_SPI_CS1, BCM2835_SPI_SPEED_8MHZ);
+RF24 radio(RPI_V2_GPIO_P1_22, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);
 
 RF24Network network(radio);
 
@@ -23,24 +23,35 @@ int main(int argc, char** argv)
 	NrfMessage msg;
 	// Refer to RF24.h or nRF24L01 DS for settings
 	radio.begin();
-	delay(5);
+	delay(100);
 	network.begin(NRF_CHANNEL_MAIN, NODE_ADDRESS);
 	radio.printDetails();
 
 	while(1)
 	{
 		network.update();
-		while ( network.available() )	// Is there anything ready for us?
+		while ( network.available() )		// Is there anything ready for us?
 		{
+			char dummy[32];
 			RF24NetworkHeader header;        // If so, grab it and print it out
+			header.type = -1;
+
 			network.peek(header);
+
+			printf("Received message, id: %05d, from node: %05o\n", header.id, header.from_node);
 
 			switch (header.type)
 			{
+			case -1:
+				// nothing to read?!
+				printf("No header...\n");
 			case MsgType_TempHum:
 				network.read(header, &msg, sizeof(msg));
-				printf("Received payload # %lu at %lu \n",payload.counter,payload.ms);
+				printf("DHT data: T=%2.3f, H=%2.3f\n\n", msg.dht.temperature, msg.dht.humidity);
 				break;
+			default:
+				int dummy_size = network.read(header, &dummy, 32);
+				printf("Unknown header type: %d. Buffer: 32, read: %d\n", header.type, dummy_size);
 			}
 
 		}
